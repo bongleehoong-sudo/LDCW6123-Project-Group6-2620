1ea2a3b9e6d4ab9391be3d60202f41fa5fca4ff1
bongl <bongleehoong@gmail.com>
2026-09-25 15:38:39 +0800

    Completing Functions
    adding helper functions and completing all planned functions

--- LDCW6123 Project Code (Antivirus).cpp
+++ LDCW6123 Project Code (Antivirus).cpp
@@ -23,7 +23,39 @@ struct HistoryRecord{
 	string result;
 };
 
+string virusNames[SIGNATURE_COUNT] = {
+	"Brain", "Stoned", "Jerusalem", "Michelangelo", "Melissa", "ILOVEYOU"
+};
+
+string virusSignatures[SIGNATURE_COUNT] = {
+	"BRAIN", "STONED", "JERU", "MICHEL", "MELISSA", "LOVE-LETTER"
+};
+
+Programs SimulatedPrograms[PROGRAM_COUNT] = {
+	{"SYSTEM Utility", "BOOT SYSTEM CLEAN", 1260, 1260, 35, "ACTIVE"},
+	{"GAME Program", "PLAYER LEVEL SCORE", 1420, 1420, 10, "ACTIVE"},
+	{"DOWNLOAD Setup", "INSTALL BRAIN PAYLOAD", 1880, 1880, 80, "ACTIVE"},
+	{"EMAIL Attachment", "DOCUMENT JERU MACRO", 1730, 1730, 70, "ACTIVE"},
+	{"PHOTO Editor", "IMAGE FILTER COLOUR", 1510, 1510, 15, "ACTIVE"},
+	{"UPDATED Tool", "SYSTEM TOOL NEW VERSION", 1200, 1450, 55, "ACTIVE"},
+	{"USB Boot Tool", "BOOT STONED SECTOR", 1650, 1650, 75, "ACTIVE"},
+	{"OFFICE Document", "REPORT MELISSA MACRO", 1340, 1340, 65, "ACTIVE"},
+	{"LOVE Letter", "LOVE-LETTER EMAIL SCRIPT". 1490, 1490, 90, "ACTIVE"},
+	{"ART Program", "DRAW PAINT CANVAS", 1380, 1380, 5, "ACTIVE"},
+	{"BACKUP Utility", "COPY ARCHIVE STORAGE", 1550, 1550, 30, "ACTIVE"},
+	{"OLD Boot Disk", "MICHEL BOOT SAMPLE", 1600, 1600, 85, "ACTIVE"}
+};
+
+HistoryRecord list[HISTORY_SIZE];
+int historyCount = 0;
+
+string getRiskLevel(int score);
+void addHistory(string scanType, string programName, string result);
+int findSignature(string programData);
+
+
 void mainMenu();
+void scanOneProgram(int programIndex, string scanType);
 void quickScan();
 void fullScan();
 void customThreatAnalysis();
@@ -99,4 +131,205 @@ void mainMenu(){
 	cout << "============================================================\n";
 }
 
+int findSignature(string programData) {
+	for (int i = 0; i < SIGNATURE_COUNT; i++) {
+		if (programData.find(virusSignatures[i]) != string::npos) {
+			return i;
+		}
+	}
+	return -1;
+}
 
+string getRiskLevel(int score){
+	if(score <= 24){
+		return "SAFE";
+	} else if (score <=49) {
+		return "LOW RISK";
+	} else if (score <= 74) {
+		return "SUSPICIOUS";
+	} else {
+		return "HIGH RISK";
+	}
+}
+
+void addHistory(string scanType, string programName, string result){
+	if(historyCount < HISTORY_SIZE){
+		historyList[historyCount].scanType = scanType;
+		historyList[historyCount].programName = programName;
+		hisotryList[historyCount].result = result;
+		historyCount++;
+	}
+}
+
+void scanOneProgram(int programIndex, string scanType){
+	int signatureIndex = findSignature(programs[programIndex].data);
+	string result;
+	
+	cout << "\nScanning: " << programs[programIndex].name << "\n";
+	
+	if(signatureIndex != -1){
+		result = "KNOWN THREAT: " + virusNames[signatureIndex];
+		cout << "Signature result : " << result << "\n";
+	} else {
+		cout << "Signature result : NO MATCH\n";
+		result = getRiskLevel(SimulatedPrograms[programIndex].heuristicScore);
+	}
+	
+	cout << "Threat score: " << SimulatedPrograms[programIndex].heuristicScore << " / 100\n";
+	cout << "Heuristic result: " << getRiskLevel(SimulatedPrograms[programIndex].heuristicScore) << "\n";
+	cout << "Status: " << SimulatedPrograms[programIndex].status << "\n";
+	
+	addHistory(scanType, SimulatedPrograms[programIndex].name, result);
+}
+
+void quickScan() {
+	cout << "--- QUICK SCAN ---\n";
+	scanOneProgram(0, "Quick");
+	scanOneProgram(2, "Quick");
+	scanOneProgram(3, "Quick");
+	scanOneProgram(8, "Quick");
+	cout << "\nQuick scan complete. \n";
+}
+
+void customThreatAnalysis() {
+	int score = 0;
+	int choice = 0;
+	
+	cout << "--- CUSTOM HEURISTIC THREAT ANALYSIS ---\n";
+	
+	cout << "\nProgram type:\n";
+	cout << "1. Official store (+0)\n";
+	cout << "2. Script (+10)\n";
+	cout << "3. Executable (+20)\n";
+	cout << "Choice: ";
+	cin << choice;
+	if(choice == 2) score += 10;
+	else if(choice == 3) score +=20;
+	
+	cout << "\nDownload score: \n";
+	cout << "1. Official store (+0)\n";
+	cout << "2. Unknown website (+15)\n";
+	cout << "3. Untrusted scource (+25)\n";
+	cout << "Choice: ";
+	cin >> choice;
+	if (choice == 2) score += 15;
+	else if (choice == 3) score += 20;
+	
+	cout << "\nObserved behaviour:\n";
+	cout << "1. Normal behaviour (+0)\n";
+	cout << "2. Changes system settings (+15)\n";
+	cout << "3. Unknown network access (+25)\n";
+	cout << "4. Disables security (+40)\n";
+	cout << "5. Encypts many documents (+45)\n";
+	cout << "Choice: ";
+	cin << choice;
+	if (choice == 2) score += 15;
+	else if (choice == 3) score += 25;
+	else if (choice == 4) score += 40;
+	else if (choice == 5) score += 45;
+	
+	cout << "\nIs the program packed or obufscated?\n";
+	cout << "1. Yes (+15)\n";
+	cout << "2. No (+0)\n";
+	cout << "Choice: ";
+	cin >> choice;
+	if(choice == 1) score += 15;
+	
+	if (score > 100) {
+		score = 100;
+	}
+	
+	cout << "\nThreat score : " << score << " / 100\n";
+	cout << "Risk Level : " << getRiskLevel(score) << "\n";
+	if(score <= 24) cout << "Recommended Action: Allow and monitor\n";
+	else if (score <=49) cout << "Recommended Action: Check the source \n";
+	else if (score <- 74) cout << "Recommended Action: Investigate carefully\n";
+	else cout << "Recommended Action: Quarantine for investigation\n";
+	
+	addHistory("Custom", "User answer", getRiskLevel(score));
+}
+
+void virusDatabase() {
+	cout << "--- Virus Database ---\n";
+	cout << "Database Version: 1.0\n";
+	cout << "Known Threats : " << SIGNATURE_COUNT << "\n\n";
+	cout << "Name\t\tSignature\n";
+	for (int i = 0; i < SIGNATURE_COUNT; i++) {
+		cout << virusNames[i] << "\t\t" << virusSignatures[i] << "\n";
+	}
+}
+
+void Quarantine() {
+	int choice = 0;
+	
+	while (choice != PROGRAM_COUNT + 1) {
+		system("cls");
+		cout << "--- QUARANTINE ---\n";
+		for (int i = 0; i < PROGRAM_COUNT; i++) {
+			cout << i + 1 << ". " << SimulatedPrograms[i].name << " [" << SimulatedPrograms[i].Status << "]\n";	
+		}
+		cout << PROGRAM_COUNT + 1 << ". Return to main menu\n";
+		
+		cout << "Enter choice: ";
+		cin >> choice;
+		
+		if (choice >= 1 && choice <= PROGRAM_COUNT) {
+			int index = choice - 1;
+			if (SimulatedPrograms[index].Status == "QUARANTINED"){
+				cout << "\n" << SimulatedPrograms[index].name << " is already QUARANTINED.\n";
+			} else {
+				SimulatedPrograms[index].Status = "QUARANTINED";
+				cout << "\n" << SimulatedPrograms[index].name << " is now QUARANTINED (simulation). \n";
+				addHistory("Action", SimulatedPrograms[index].name, "QUARANTINED");
+			}
+			cout << "\n";
+			system("pause");
+		}
+	}
+}
+
+void integrityCheck() {
+	int choice = 0;
+	
+	while(choice != PROGRAM_COUNT + 1) {
+		system("cls");
+		cout << "--- INTEGRITY CHECK ---\n";
+		for (int i = 0; i < PROGRAM_COUNT; i++) {
+			cout << i + 1 << ". " << SimulatedPrograms[i].name << "\n";
+		}
+		cout << PROGRAM_COUNT + 1 << ". Return to main menu\n";
+		
+		cout << "Enter choice: ";
+		cin >> choice;
+		
+		if (choice >= 1 && choice <= PROGRAM_COUNT) {
+			int index = choice - 1;
+			cout << "\nOriginal checksum: " << SimulatedPrograms[index].originalChecksum << "\n";
+			cout << "Current checksum: " << SimulatedPrograms[index].currentChecksum << "\n";
+			
+			if (SimulatedPrograms[index].originalChecksum == SimulatedPrograms[index].currentChecksum) {
+				cout << "Result: No change detected.\n";
+				addHistory("Integrity", SimulatedPrograms[index].name, "UNCHANGED");
+			}else {
+				cout << "Result: WARNING! Program has been modified.\n";
+				addHistory("Integrity", SimulatedPrograms[index].name, "MODIFIED");
+			}
+			cout << "\n";
+			system("pause");
+		}
+	}
+}
+
+void history() {
+	cout << "--- Current Session History ---\n";
+	if(historyCount == 0) {
+		cout << "No scans or actions recorded yet.\n";
+		return;
+	}
+	
+	for (int i = 0; i< historyCount; i++) {
+		cout << i + 1 << ". [" << historyList[i].scanType << "] "
+			 <<historyList[i].ProgramName << " -> "
+			 <<historyList[i].result << "\n";
+	}
+}
